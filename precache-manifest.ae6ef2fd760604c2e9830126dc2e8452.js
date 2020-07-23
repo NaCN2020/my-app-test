be3866d1ee9ad8b471827803229b8a94ebae51a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-23 09:15:20 +0000

    Deploy NaCN2020/my-app-test to github.com/NaCN2020/my-app-test.git:gh-pages

---
+++ precache-manifest.ae6ef2fd760604c2e9830126dc2e8452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f0cd26bb6427a6ea33592d178e6f41",
+    "url": "/my-app-test/index.html"
+  },
+  {
+    "revision": "506b33d16c394b23b4ac",
+    "url": "/my-app-test/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/my-app-test/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-test/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506b33d16c394b23b4ac",
+    "url": "/my-app-test/static/js/main.13e0a4a2.chunk.js"
+  },
+  {
+    "revision": "aa359585b1eb4dd0618e",
+    "url": "/my-app-test/static/js/runtime-main.8d45c4c5.js"
+  }
+]);
